fa3d0c1c3612dce9ad770db89f7e62690607a5ce
Enrique9812 <120746430+Enrique9812@users.noreply.github.com>
2025-03-15 12:16:29 -0600

    Update constantes.php
    Se agrego una constante.

--- config/constantes.php
+++ config/constantes.php
@@ -4,5 +4,6 @@
 define('URL_BASE_PROYECT', 'http://portalproveedores.localhost');
 define('LOG_FILE', '../logs/debug.log');
 define('LOG_FILE_BD', '../logs/debug_BD.log');
+define('LOG_SYSTEM', '../logs/sistema/');
 
 ?>
