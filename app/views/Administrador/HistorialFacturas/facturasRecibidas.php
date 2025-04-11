86365ca5154a694a662520fd288a4b3121e04765
Enrique9812 <120746430+Enrique9812@users.noreply.github.com>
2025-04-11 16:47:27 -0600

    Actualización En Historial
    Se actualizo el historial de facturas.

--- app/views/Administrador/HistorialFacturas/facturasRecibidas.php
+++ app/views/Administrador/HistorialFacturas/facturasRecibidas.php
@@ -1,142 +1,157 @@
 <?php
-
-use App\Globals\FuncionesBasicas\FuncionesBasicasController;
-
-$funcionesBase = new FuncionesBasicasController();
-
 $debug = 0;
 
 if ($debug == 1) {
-    echo 'Contenido de menuData:';
-    var_dump($menuData);
-    echo '<br><br>Contenido de areaData:';
-    var_dump($areaData);
-    echo '<br><br>Contenido de areaLink:';
-    var_dump($areaLink);
-    echo '<br><br>Contenido de _SESSION:';
-    var_dump($_SESSION);
-    echo '<br><br>Request-URI: ' . $_SERVER['REQUEST_URI'] . '<br>Contenido de piezasURL:';
-    var_dump($piezasURL);
-    echo '<br><br>Ruta del MenuActual: ' . $rutaMenu . '<br><br>Contenido de datosPagina:';
-    var_dump($datosPagina);
-    echo '<br><br>Contenido de historialFacturas';
-    var_dump($historialFacturas['data']);
+    echo 'Contenido de areaData:' . PHP_EOL;
+    var_dump($listaCompras);
 }
 
-
-
 ?>
-
-<?php
-if ($historialFacturas['success'] == false) {
-?>
-    <div class="alert alert-info">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
-        <h3 class="text-info"><i class="fa fa-exclamation-circle"></i> Atención</h3> No Hay Facturas Recientes.
-    </div>
-<?php
-} else {
-?>
-    <table class="table table-sm" id="tableAprobacionesNa">
+<div class="table-responsive">
+    <table class="table table-sm table-striped" id="tableListaCompras" style="width:100%">
         <thead>
             <tr>
-                <th>Acuse</th>
-                <th>Clase Docto</th>
-                <th>Fecha Registro</th>
-                <th>País</th>
+                <th class="text-center"># Acuse</th>
+                <th class="">Tipo</th>
                 <th>Proveedor</th>
                 <th>Orden Compra</th>
-                <th>Folio Ref</th>
-                <th>Receptor</th>
-                <th>Monto</th>
-                <th>Estatus Físcal</th>
-                <th>Estatus Contable</th>
-                <th>Factura</th>
-                <th>Complemento</th>
-                <th>Acciones</th>
+                <th>No Recepcion</th>
+                <th>Moneda</th>
+                <th>Fecha Recepción</th>
+                <th>Folio Interno</th>
+                <th>Estatus Fiscal</th>
+                <th>Ver</th>
             </tr>
         </thead>
         <tbody>
             <?php
-            foreach ($historialFacturas['data'] as $key => $historial) {
+            if (!empty($listaCompras)) {
+                foreach ($listaCompras as $row) {
+                    switch ($row['claseDocto']) {
+                        case 'WE':
+                            $claseDocto = 'FACT';
+                            break;
 
-                switch ($historial['ClaseDocto']) {
-                    case 'KA':
-                        $claseDocto = 'ANT';
-                        break;
+                        case 'KA':
+                            $claseDocto = '<b>ANT</b>';
+                            break;
 
-                    case 'RZ':
-                        $claseDocto = 'CONS';
-                        break;
+                        case 'RZ':
+                            $claseDocto = '<b>CONS</b>';
+                            break;
 
-                    default:
-                        $claseDocto = 'FACT';
-                        break;
+                        default:
+                            $claseDocto = '<span class="text-danger"> NO_DEF </span>';
+                            break;
+                    }
+
+                    switch ($row['estatus']) {
+                        case '1':
+                            $statContable = '<center class="text-danger"><i class="md md-close"></i></center>';
+                            $txtColor = '';
+                            $bgColor = '';
+                            break;
+
+                        case '2':
+                            $statContable = '<center class="text-success"><i class="fas fa-check"></i></center>';
+                            $txtColor = '';
+                            $bgColor = '';
+                            break;
+
+                        case '3':
+                            $statContable = '<center class="text-danger"><i class="md md-close"></i></center>';
+                            $txtColor = 'text-danger';
+                            $bgColor = 'danger';
+                            break;
+
+                        case '4':
+                            $statContable = '<center class="text-danger"><i class="md md-close"></i></center>';
+                            $txtColor = 'text-danger';
+                            $bgColor = 'danger';
+                            break;
+
+                        default:
+                            $txtColor = '';
+                            $bgColor = '';
+                            break;
+                    }
+
+                    $valida = '<center class="text-success"><i class="fas fa-check"></i></center>';
+                    $cantCharRecp = strlen($row['noRecepcion']);
+                    if ($cantCharRecp > 25) {
+                        $recepciones = substr($row['noRecepcion'], 0, 25) . '...';
+                    } else {
+                        $recepciones = $row['noRecepcion'];
+                    }
+
+                    echo '<tr class="' . $txtColor . ' ' . $bgColor . '" >
+                    <td class="text-center">' . $row['acuse'] . '</td>
+                    <td>' . $claseDocto . '</td>
+                    <td>' . $row['RazonSocial'] . '</td>
+                    <td>' . $row['ordenCompra'] . '</td>
+                    <td>' . $recepciones . '</td>
+                    <td>' . $row['TipoMonedaFac'] . '</td>
+                    <td>' . $row['fechaReg'] . '</td>
+                    <td>' . $row['referencia'] . '</td>
+                    <td>' . $valida . ' </td>
+                    <td> <button class="btn btn-sm btn-success" onClick="detalleCompra(\'' . $row['acuse'] . '\',' . $row['IdProveedor'] . ');"><i class="text-white icon-doc"></i></button> </td>
+                    </tr>';
                 }
-
-                if ($historial['Validada'] == 1) {
-                    $estatusFiscal = '<i class="fas fa-check text-default" data-toggle="tooltip" data-placement="top" title="No Se Valida Fiscalmente"></i>';
-                } else {
-                    $estatusFiscal = '<i class="fas fa-check text-success" data-toggle="tooltip" data-placement="top" title="Valida Fiscalmente"></i>';
-                }
-
-                $estatusContable = $historial['EstatusContable'];
-                $basePago = $historial['BasePago'];
-                $fehcaVence = $historial['FechaVence'];
-                $msjEstatusContable = '';
-                switch ($estatusContable) {
-                    case '1':
-                        $msjEstatusContable = 'En Espera De Validación';
-
-
-                        break;
-                    case '2':
-
-                        $msjEstatusContable = 'Se A Contabilizado';
-
-                        break;
-                    case '3':
-                        $msjEstatusContable = 'Esta Factura Fue Regresada';
-
-                        break;
-                    case '4':
-                        $msjEstatusContable = 'Factura Eliminada Por El Usuario';
-
-                        break;
-                    default:
-                        $msjEstatusContable = 'Estatus Contable No Identificado';
-                        break;
-                }
-                
-                if ($historial['Pais'] == 'MX') {
-                   $pdfXml = '';
-                }
-
-            ?>
-                <tr>
-                    <a target="_blank" href="../<?= $historial['FacXML'] ?>"></a>
-                    <td><?= $historial['Acuse']; ?></td>
-                    <td><?= $claseDocto; ?></td>
-                    <td><?= $historial['FechaReg']; ?></td>
-                    <td><?= $historial['Pais']; ?></td>
-                    <td><?= $historial['NombreProveedor']; ?></td>
-                    <td><?= $historial['OC']; ?></td>
-                    <td><?= $historial['Referencia']; ?></td>
-                    <td><?= $historial['RfcReceptor']; ?></td>
-                    <td><?= $historial['MontoFactura']; ?></td>
-                    <td class="text-center"><?= $estatusFiscal; ?></td>
-                    <td> <?= $msjEstatusContable; ?></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            <?php
             }
             ?>
-
         </tbody>
     </table>
-<?php
-}
-?>
+</div>
+<script>
+    $('#tableListaCompras').DataTable({
+        iDisplayLength: 25,
+        responsive: false,
+        fixedColumns: true,
+        fixedHeader: true,
+        scrollCollapse: true,
+        autoWidth: true,
+        bSort: true,
+        order: [
+            [0, "desc"]
+        ],
+        dom: 'Blfrtip',
+        lengthMenu: [
+            [10, 25, 50, -1],
+            [10, 25, 50, "Todo"]
+        ],
+        info: true,
+        buttons: [{
+                extend: 'pdfHtml5',
+                className: 'btn btn-pdf bg-pyme-primary text-white',
+                orientation: 'landscape',
+                pageSize: 'LEGAL',
+                text: "Pdf",
+            },
+
+            {
+                extend: 'csvHtml5',
+                className: 'btn btn-pdf bg-pyme-primary text-white',
+                text: "Csv",
+                exportOptions: {
+                    columns: ":not(.no-exportar)"
+                }
+            },
+            {
+                extend: 'excelHtml5',
+                className: 'btn btn-pdf bg-pyme-primary text-white',
+                text: "Excel",
+                exportOptions: {
+                    columns: ":not(.no-exportar)"
+                }
+            },
+            {
+                extend: 'copy',
+                className: 'btn btn-pdf bg-pyme-primary text-white',
+                text: "Copiar",
+                exportOptions: {
+                    columns: ":not(.no-exportar)"
+                }
+            }
+        ]
+    });
+</script>
